debfdd511af54e6de77ec39d7725e16657c1eb7f
DESKTOP-F3DRBVV\Vinh <loc.huynh@namlongsoft.net>
2021-02-24 16:03:53 +0700

    update title, meta, name

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -489,7 +489,7 @@ const _ = require('lodash')
 //       ],
 //     })
 //     res.render('user/index', {
-//       title: 'Ngọc Quốc Computer - Sửa chữa lắp đặt thiết bị vi tính',
+//       title: 'TeslaPower VN - Sửa chữa lắp đặt thiết bị vi tính',
 //       newProducts,
 //       topSellProducts,
 //       topViewProducts,
@@ -498,7 +498,7 @@ const _ = require('lodash')
 //     })
 //   } catch (error) {
 //     res.render('user/index', {
-//       title: 'Ngọc Quốc Computer - Sửa chữa lắp đặt thiết bị vi tính',
+//       title: 'TeslaPower VN - Sửa chữa lắp đặt thiết bị vi tính',
 //       newProducts: [],
 //       topSellProducts: [],
 //       topViewProducts: [],
@@ -617,10 +617,10 @@ router.get('/', async (_, res) => {
       where: { isActive: true },
     })
   
-    res.render('user/index', { title: 'Trang Chủ - Goodwe VN', inverters, solarPins, solarFans, solarLeds, solarCameras })
+    res.render('user/index', { title: 'Trang Chủ - TeslaPower  VN', inverters, solarPins, solarFans, solarLeds, solarCameras })
   } catch (error) {
     res.render('user/index', {
-      title: 'Trang Chủ - Goodwe VN',
+      title: 'Trang Chủ - TeslaPower  VN',
       inverters: [],
       solarPins: [],
       solarFans: [],
