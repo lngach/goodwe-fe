59f927c9cb07359051e68f736f94aa130ce44fed
lngach <kugack.darkness@gmail.com>
2021-06-30 14:32:59 +0700

    Add ssl

--- src/routes/ssl-verify/index.js
+++ src/routes/ssl-verify/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/.well-known/pki-validation/', (_, res) => {
+router.get('/.well-known/pki-validation/BD551E1407E3D4A83B691D3102413886.txt', (_, res) => {
   res.sendFile(path.resolve('BD551E1407E3D4A83B691D3102413886.txt'))
 })
 
